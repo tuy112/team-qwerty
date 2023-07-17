f1043c8264c680629a8de52d0a17ce865600fa24
gajigaji0401 <gajigaji04@naver.com>
2023-07-17 14:40:51 +0900

    초기 세팅

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 
-const path = require('path');
+const path = require("path");
 
 // HTML, CSS
-app.use(express.static(path.join(__dirname, 'assets')));
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'assets', 'index.html'));
+app.use(express.static(path.join(__dirname, "assets")));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "assets", "index.html"));
 });
 
 app.listen(port, () => {
-  console.log(port, '포트가 열렸습니다~^^');
+  console.log(port, "포트가 열렸습니다");
 });
