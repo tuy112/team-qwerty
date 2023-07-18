fe7db0d20bd10e709093c016de265eef9184da21
KimJaeHyeok <master_catch@naver.com>
2023-07-18 17:24:12 +0900

    2023.07.18 (JaeH)

--- app.js
+++ app.js
@@ -2,10 +2,24 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-const storeRouter = require('./routes/store.route');
-const menuRouter = require('./routes/menu.route');
-
 const path = require('path');
+
+// cookie parser
+const cookieParser = require("cookie-parser");
+
+const storeRouter = require("./routes/storeRoute.js");
+const checkRouter = require("./routes/checkRoute.js");
+const menuRouter = require("./routes/menuRoute.js");
+
+// Middleware ==================================================
+app.use(express.json()); // req.body parser
+app.use(cookieParser()); // cookie parser
+// app.use(cors()); // front-back connect
+
+// localhost:3000/api/
+app.use('/api', [storeRouter, checkRouter, menuRouter]);
+
+// Middleware ==================================================
 
 // HTML, CSS
 app.use(express.static(path.join(__dirname, 'assets')));
