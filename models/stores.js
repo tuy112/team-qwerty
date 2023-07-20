ff439effa43a17c05e0af0fc4bbc84cb7ed3ef13
kme-developer <electruc0095@gmail.com>
2023-07-20 20:40:26 +0900

    2023.07.20(KJY)
    axios get 구현
    음식점 자료 받아오기 구현
    기타 오류 수정

--- models/stores.js
+++ models/stores.js
@@ -1,62 +1,62 @@
 'use strict';
 const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-    class Stores extends Model {
-        /**
-         * Helper method for defining associations.
-         * This method is not a part of Sequelize lifecycle.
-         * The `models/index` file will call this method automatically.
-         */
-        static associate(models) {
-            // define association here
-            // Stores와 Menus는 일대다 관계
-            this.hasMany(models.Menus, {
-                sourceKey: 'storeId',
-                foreignKey: 'StoreId',
-            });
-        }
+  class Stores extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      // Stores와 Menus는 일대다 관계
+      this.hasMany(models.Menus, {
+        sourceKey: 'storeId',
+        foreignKey: 'storeId',
+      });
     }
-    Stores.init(
-        {
-            storeId: {
-                allowNull: false,
-                autoIncrement: true,
-                primaryKey: true,
-                type: DataTypes.INTEGER,
-            },
-            email: {
-                allowNull: false,
-                type: DataTypes.STRING,
-            },
-            password: {
-                allowNull: false,
-                type: DataTypes.STRING,
-            },
-            storeName: {
-                allowNull: false,
-                type: DataTypes.STRING,
-            },
-            storeImage: {
-                type: DataTypes.STRING,
-            },
-            totalRating: {
-                type: DataTypes.INTEGER,
-            },
-            createdAt: {
-                allowNull: false, // NOT NULL
-                type: DataTypes.DATE,
-                defaultValue: DataTypes.NOW,
-            },
-            updatedAt: {
-                allowNull: false, // NOT NULL
-                type: DataTypes.DATE,
-                defaultValue: DataTypes.NOW,
-            },
-        },
-        {
-            sequelize,
-            modelName: 'Stores',
-        },
-    );
-    return Stores;
+  }
+  Stores.init(
+    {
+      storeId: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      email: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      password: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      storeName: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      storeImage: {
+        type: DataTypes.STRING,
+      },
+      totalRating: {
+        type: DataTypes.INTEGER,
+      },
+      createdAt: {
+        allowNull: false, // NOT NULL
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+      },
+      updatedAt: {
+        allowNull: false, // NOT NULL
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+      },
+    },
+    {
+      sequelize,
+      modelName: 'Stores',
+    },
+  );
+  return Stores;
 };
