62d26cdf70c203ef05df88b5248ab1f62ed2cc0b
KimJaeHyeok <master_catch@naver.com>
2023-07-21 15:02:48 +0900

    2023.07.21 (JaeH)

--- middlewares/ImgUploadMiddleware.js
+++ middlewares/ImgUploadMiddleware.js
@@ -6,15 +6,15 @@ const path = require('path');
 const s3Client = new S3Client({
     region: 'ap-northeast-2',
     credentials: {
-        accessKeyId: '',
-        secretAccessKey: '',
+        accessKeyId: 'AKIA5F4OE7F2N5DX6K7C',
+        secretAccessKey: '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',
     },
 });
 
 const upload = multer({
   storage: multerS3({
     s3: s3Client,
-    bucket: '',
+    bucket: 'jstol-bucket',
     acl: 'public-read',
     contentType: multerS3.AUTO_CONTENT_TYPE,
     key: function (req, file, cb) {
